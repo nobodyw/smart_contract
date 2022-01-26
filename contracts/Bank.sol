0eab85a4805cf648f2cc256fb567ee09e6bb162e
nobodyw <michael.wong@epitech.eu>
2022-01-26 21:25:46 +0100

    Add Event to Contract Bank + add test_bank.js

--- contracts/Bank.sol
+++ contracts/Bank.sol
@@ -10,7 +10,8 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 */
 contract Bank is Ownable{
     mapping(address => uint) _balances ;
-
+    event Deposit(uint _amount);
+    event Transfer(address payable _recipient, uint _amount);
 /*
 * @notice deposit allows to deposit a value in the contract
 * @dev the funds used are fictitious funds and in no case ERC-20
@@ -18,6 +19,7 @@ contract Bank is Ownable{
     function deposit(uint _amount) public payable{
         require(_amount > 0, "The amount is to low");
         _balances[msg.sender] += _amount;
+        emit Deposit(_amount);
     }
 
 /*
@@ -34,10 +36,11 @@ contract Bank is Ownable{
     function transfer(address payable _recipient, uint _amount) public payable{
         require(_amount > 0, "The amount is to low");
         require(msg.sender != _recipient,'transfer only works with an account other');
-        require(_balances[msg.sender] >= _amount,"vous manquez de fond");
-        require(_balances[_recipient] + _amount >= _balances[_recipient],"nous avons un probleme lors de votre transfer d'argent");
+        require(_balances[msg.sender] >= _amount,"You lack funds");
+        require(_balances[_recipient] + _amount >= _balances[_recipient],"We have a problem during your money transfer");
 
         _balances[msg.sender] -= _amount;
         _balances[_recipient] += _amount;
+        emit Transfer(_recipient, _amount);
     }
 }
---
+++ test/test_bank.js
@@ -0,0 +1,38 @@
+const { expectRevert } = require('@openzeppelin/test-helpers');
+const Assert = require("assert");
+const Web3 = require('web3');
+
+const BankContract = artifacts.require("./Bank.sol");
+contract("Bank", function(accounts){
+    const owner = accounts[0];
+    const userBank = accounts[1];
+
+    context("must add funds in the bank and withdraw them", function(){
+        beforeEach(async function (){
+            Bank = await BankContract.new({from: owner});
+            await Bank.deposit(Web3.utils.toBN(10 * (10 **18)),{from:owner});
+            await Bank.deposit(Web3.utils.toBN(5 * (10 **18)),{from:userBank});
+        });
+
+        it("Owner and userBank should deposit 10p18 and 5p18 in his bank account",async() =>{
+            await expectRevert(Bank.deposit(0, {from:owner}),'amount is to low');
+            Assert.equal(await Bank.balanceOf(owner,{from:owner}),
+                10*(10**18),'error in deposit Owner');
+            Assert.equal(await Bank.balanceOf(userBank,{from:userBank}),
+                5*(10**18),'error in deposit userBank');
+        });
+
+        it("Owner should transfer 1p18 to userBank", async() => {
+            await expectRevert(Bank.transfer(userBank,0,{from:owner}),
+                'amount is to low');
+            await expectRevert(Bank.transfer(owner,100,{from:owner}),
+                'transfer only works with an account other');
+            await Bank.transfer(userBank,Web3.utils.toBN(1 * (10 ** 18)),{from:owner});
+
+            Assert.equal(await Bank.balanceOf(owner,{from:owner}),
+                9 *(10**18),'error in transfer Owner');
+            Assert.equal(await Bank.balanceOf(userBank,{from:userBank}),
+                6*(10**18),'error in transfer Owner');
+        });
+    });
+});
