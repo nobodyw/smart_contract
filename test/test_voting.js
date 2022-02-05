0395e3c8beb0659db9621beb3d7da9cc7a72b00b
nobodyw <michael.wong@epitech.eu>
2022-02-05 02:01:50 +0100

    tests workflow Voting

--- test/test_voting.js
+++ test/test_voting.js
@@ -35,7 +35,68 @@ contract("Voting", function(accounts){
             await Voting.countVote({from:owner});
         });
         it("test",async function(){
-            console.log(await Voting.Winner.call());
+            // console.log(await Voting.Winner.call());
+        });
+    });
+
+    context("Test the workflow", function() {
+        beforeEach(async function(){
+            Voting = await VotingContract.new({from:owner});
+            await Voting.registerVoter(user1,{from:owner});
+        });
+        it('Fail if function registerVoter call after startProposals', async function(){
+            await Voting.startProposals({from:owner});
+
+            await expectRevert(Voting.registerVoter(user1,{from:owner}),'cannot add new voters');
+        });
+        it('Fail if function voterAddProposal call after endProposals',async function(){
+            await Voting.startProposals({from:owner});
+            await Voting.endProposals({from:owner});
+
+            await expectRevert(Voting.registerVoter(user1,{from:owner}),'cannot add new voters');
+            await expectRevert(Voting.startProposals({from:owner}),'cannot start session proposals');
+            await expectRevert(Voting.voterAddProposal("test description",{from:user1}),'cannot add proposal');
+        });
+        it('Fail if function endProposals call after startVote',async function(){
+            await Voting.startProposals({from:owner});
+            await Voting.endProposals({from:owner});
+            await Voting.startVote({from:owner});
+
+            await expectRevert(Voting.registerVoter(user1,{from:owner}),'cannot add new voters');
+            await expectRevert(Voting.startProposals({from:owner}),'cannot start session proposals');
+            await expectRevert(Voting.voterAddProposal("test description",{from:user1}),'cannot add proposal');
+            await expectRevert(Voting.endProposals({from:owner}),'cannot end session proposals');
+        });
+        it('Fail if function vote call after endVote',async function(){
+            await Voting.startProposals({from:owner});
+            await Voting.endProposals({from:owner});
+            await Voting.startVote({from:owner});
+            await Voting.endVote({from:owner});
+
+            await expectRevert(Voting.registerVoter(user1,{from:owner}),'cannot add new voters');
+            await expectRevert(Voting.startProposals({from:owner}),'cannot start session proposals');
+            await expectRevert(Voting.voterAddProposal("test description",{from:user1}),'cannot add proposal');
+            await expectRevert(Voting.endProposals({from:owner}),'cannot end session proposals');
+            await expectRevert(Voting.startVote({from:owner}),'cannot start session vote');
+            await expectRevert(Voting.vote(0,{from:user1}),'cannot vote');
+        });
+        it('failed if a workflow function can be called after the votes have been counted',async function(){
+            await Voting.startProposals({from:owner});
+            await Voting.voterAddProposal('test description',{from:user1});
+            await Voting.endProposals({from:owner});
+            await Voting.startVote({from:owner});
+            await Voting.vote(0,{from:user1});
+            await Voting.endVote({from:owner});
+            await Voting.countVote({from:owner});
+
+            await expectRevert(Voting.registerVoter(user1,{from:owner}),'cannot add new voters');
+            await expectRevert(Voting.startProposals({from:owner}),'cannot start session proposals');
+            await expectRevert(Voting.voterAddProposal("test description",{from:user1}),'cannot add proposal');
+            await expectRevert(Voting.endProposals({from:owner}),'cannot end session proposals');
+            await expectRevert(Voting.startVote({from:owner}),'cannot start session vote');
+            await expectRevert(Voting.vote(0,{from:user1}),'cannot vote');
+            await expectRevert(Voting.endVote({from:owner}),'cannot stop vote session');
+            await expectRevert(Voting.countVote({from:owner}),'cannot count vote');
         });
     });
 });
